b44e382cff76e8ce9ec37ecd9b2f611ba88dd305
EdenGebremedhin <edhabtetsion@gmial.com>
2024-06-23 20:11:44 +0300

    project number 5

---
+++ Stop Watch/index.js
@@ -0,0 +1,12 @@
+let secondsElapsed = 0;
+let interval = null; // nothing or empty
+const time = document.getElementById("time")
+
+
+function startClock(){
+    
+}
+
+function stopClock(){}
+
+function resetClock(){}
